7cf6aac9bf24bcf7e802f3afca95cf1d8b8e060e
Ana Blanco Mota <ablanco-@c2r7s2.42madrid.com>
2023-02-20 15:11:40 +0100

    an

--- move.c
+++ move.c
@@ -6,7 +6,7 @@
 /*   By: ablanco- <ablanco-@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/02/16 17:49:43 by ablanco-          #+#    #+#             */
-/*   Updated: 2023/02/17 15:19:40 by ablanco-         ###   ########.fr       */
+/*   Updated: 2023/02/20 13:56:15 by ablanco-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,18 +14,23 @@
 
 void	ft_move(t_sl *sl, int dx, int dy)
 {
-	sl->map.full_map[sl->pj.y_pj][sl->pj.x_pj] = '0';
-	sl->map.full_map[sl->pj.y_pj + dy][sl->pj.x_pj + dx] = 'P';
-	sl->pj.x_pj += dx;
-	sl->pj.y_pj += dy;
-	img_to_map(&(sl->img), &(sl->vars), &(sl->map));
+	if ((sl->map.full_map[sl->pj.y_pj + dy][sl->pj.x_pj + dx] != '1'))
+	{
+		sl->map.full_map[sl->pj.y_pj][sl->pj.x_pj] = '0';
+		sl->map.full_map[sl->pj.y_pj + dy][sl->pj.x_pj + dx] = 'P';
+		sl->pj.x_pj += dx;
+		sl->pj.y_pj += dy;
+		img_to_map(&(sl->img), &(sl->vars), &(sl->map));
+	}
 }
 
 int	ft_input(int keycode, t_sl *sl)
 {
 	if (keycode == 53)
 		exit(1);
-	if ((keycode == 0 || keycode == 123) && (sl->map.full_map[sl->pj.y_pj][sl->pj.x_pj - 1] != 1))
+	printf("%c\n", sl->map.full_map[sl->pj.y_pj][sl->pj.x_pj - 1]);
+
+	if (keycode == 0 || keycode == 123) //&& !(sl->map.full_map[sl->pj.y_pj][sl->pj.x_pj - 1] != 1))
 		ft_move(sl, -1, 0);
 	if (keycode == 2 || keycode == 124)
 		ft_move(sl, 1, 0);
